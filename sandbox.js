e7deac0fb3f47d32c7c530889f296d9c28d143b5
rmariano34 <ronna.mariano@gmail.com>
2024-01-08 13:39:04 -0600

    if statments

--- sandbox.js
+++ sandbox.js
@@ -1,40 +1,19 @@
-// for loops
+// // if statements
 
-//for(let i = 0; i < 5; i++){
- //   console.log('in loop:', i);
-//}
+// const age= 25;
 
-//console.log('loop finished');
-
-//const names = ['shaun', 'mario', 'luigi'];
-
-//for(let i= 0; i < names.length; i++){
-    //console.log(names[i]);
-    //let html = `<div>${names[i]}</div>`;
-    //console.log(html);
-//}
-
-// While Loops
-//const names = ['shaun', 'mario', 'luigi'];
-//let i = 0;
-
-//while (i < 5){
-   // console.log('in loop: ', i);
-    //i++;
-//}
-
-//let i = 0;
-//while(i < names.length){
-    //console.log(names[i]);
-    //i++;
-//}
-
-// do while loops
-let i = 5;
-
-do{
-    console.log('val of i is: ', i);
-    i++;
-}
-while(i < 5);
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
   
+// const ninjas = ['shuan', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length >4){
+//     console.log("thats a lot of ninjas");
+// }
+
+const password = 'p@ssword';
+
+if(password.length >= 8){
+    console.log('that password is long enough!');
+}
